6a4f292a2a8b322077ac2d05d7fff5635c586cf4
huang <huang@123.com>
2021-12-14 14:13:04 +0800

    第三次进行数据的提交Test2。。。。

---
+++ TestModule1/src/com/huanghuang/Test2.java
@@ -0,0 +1,12 @@
+package com.huanghuang;
+
+/**
+ * @Auther: huanghuang
+ * @Date: 2021/12/14 - 12 - 14 - 14:12
+ * @Description: com.huanghuang
+ * @version: 1.0
+ */
+public class Test2 {
+    private int age;
+    private String name;
+}
